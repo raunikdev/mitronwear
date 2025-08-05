add8f9e4bae1d62a9ded33bd4a33faef9b9155bc
Raunik Rai <raunikdevelop@gmail.com>
2025-08-05 15:22:48 +0530

    imporved the spacing

--- src/components/tshirtcard.jsx
+++ src/components/tshirtcard.jsx
@@ -2,25 +2,22 @@
 
 import './css/tshirtcard.css'
 
-function Tshirtcard (prop){
-    return(
+function Tshirtcard(prop) {
+    return (
         <>
             <div className="card-tshirt">
-                <div className='image-div' style={{backgroundImage: `url(${prop.front})`}}>
-                    <div className="image-back" style={{backgroundImage: `url(${prop.back})`}} >
+                <div className='image-div' style={{ backgroundImage: `url(${prop.front})` }}>
+                    <div className="image-back" style={{ backgroundImage: `url(${prop.back})` }} >
 
                     </div>
                 </div>
                 <a href={prop.link}>
                     <div className='name-price'>
                         <p className='heading-tshirt'><b>{prop.heading}</b></p>
-                        <p className="price"><b><span className='original-price'>{prop.originalPrice}</span>{prop.price}</b></p> 
+                        <p className="price"><b><span className='original-price'>{prop.originalPrice}</span>{prop.price}</b></p>
                     </div>
 
                 </a>
-
-                
-
             </div>
         </>
     )
